427fa93d857a59e1295c3d249c53dda6df5be43b
maojun <18362962855>
2020-08-10 09:30:21 +0800

    Signed-off-by: maojun <18362962855>

---
+++ 博客文章/mongodb/数据示例.js
@@ -0,0 +1,30 @@
+[
+	{
+		title:'mongodb 数据新增实践',
+		authorName:'kuafu21C',
+		tags:['mongodb','数据库','demo','insert'],
+		content:'insert',
+		publishTime:new Date(),
+		comments:[
+			{
+				userName:'路人甲',
+				commentTime:new Date(),
+				content:''
+			}
+		]
+	},
+	{
+		title:'mongodb 数据查询实践',
+		authorName:'kuafu21C',
+		tags:['mongodb','数据库','demo','query'],
+		content:'query',
+		publishTime:new Date(),
+		comments:[
+			{
+				userName:'路人乙',
+				commentTime:new Date(),
+				content:'很好'
+			}
+		]
+	}
+]
